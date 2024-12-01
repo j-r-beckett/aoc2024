824ec51ff0171d5dad678e1b7da4e04882738973
Jimmy Beckett <jimmybeckett@microsoft.com>
2024-11-30 22:38:28 -0600

    First commit

---
+++ 1/Program.fs
@@ -0,0 +1,15 @@
+﻿let rec reverse lst = 
+    match lst with
+    | head::tail -> (reverse tail) @ [head]
+    | [] -> []
+
+let rec firstDigit chars = 
+    match chars with
+    | head::tail -> if System.Char.IsDigit head then string head else firstDigit tail
+    | [] -> invalidOp "no digits"
+
+let calibrationVal chars = int (firstDigit chars + (firstDigit (reverse chars)))
+
+
+
+System.IO.File.ReadLines("input.dat") |> List.ofSeq |> List.map Seq.toList |> List.map calibrationVal |> List.sum |> printfn "%A"
