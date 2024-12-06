43c7446eae24cb00675e9f0775d08c9f53ec6434
jeckett <jeckett@cs.washington.edu>
2024-12-05 23:41:25 -0800

    Failed attempt at part 2

--- 6/Program.fs
+++ 6/Program.fs
@@ -49,6 +49,52 @@ let rec moveGuard (map: array<array<char>>) (guardRow: int, guardCol: int) =
             moveGuard map (stepGuardForward ()) 
 
 
+let rec isLoop (map: array<array<char>>) (guardRow: int, guardCol: int) (visited: Set<string>) =
+    let direction row col = match map[row][col] with
+                            | '^' -> -1, 0
+                            | '>' -> 0, 1
+                            | 'v' -> 1, 0
+                            | '<' -> 0, -1
+                            | _ -> raise (System.InvalidOperationException "unkown guard direction")
+
+    let isBlocked (row: int) (col: int) (direction: int * int) = map[row + (fst direction)][col + (snd direction)] = '#'
+
+    let willStepOffMap row col direction = 
+        let nextRow, nextCol = row + (fst direction), col + (snd direction)
+        nextRow <= -1 || nextRow >= map.Length || nextCol <= -1 || nextCol >= map[0].Length
+
+    let rotateGuard () =
+        let newGuard = match map[guardRow][guardCol] with
+                        | '^' -> '>'
+                        | '>' -> 'v'
+                        | 'v' -> '<'
+                        | '<' -> '^'
+                        | _ -> raise (System.InvalidOperationException "unkown guard direction")
+        Array.set map[guardRow] guardCol newGuard
+
+    let stepGuardForward () =
+        let guardDirection = direction guardRow guardCol
+        let newGuardRow, newGuardCol = guardRow + (fst guardDirection), guardCol + (snd guardDirection)
+        Array.set map[newGuardRow] newGuardCol (map[guardRow][guardCol])
+        Array.set map[guardRow] guardCol '.'
+        newGuardRow, newGuardCol
+
+    let guardDirection = direction guardRow guardCol
+    if willStepOffMap guardRow guardCol guardDirection
+    then 
+        false
+    else 
+        let key = (string guardRow) + (string guardCol) + (string (map[guardRow][guardCol]))
+        if Set.contains  key visited
+        then true
+        else
+            if isBlocked guardRow guardCol guardDirection
+            then 
+                rotateGuard () |> ignore
+                isLoop map (guardRow, guardCol) visited
+            else 
+                isLoop map (stepGuardForward ()) (Set.add key visited)
+
 let findGuard (map: array<array<char>>) =
     // let containsGuard row = Array.contains map[row] "^" || Array.contains map[row] ">" || Array.contains map[row] "v" || Array.contains map[row] "<"
     // let guardCol = Array.find
@@ -77,14 +123,48 @@ let countXs (map: array<array<char>>) =
     let countXsInRow row = map[row] |> Array.fold (fun sumSoFar c -> sumSoFar + (if c = 'X' then 1 else 0)) 0
     [0..map.Length - 1] |> List.map countXsInRow |> List.sum
 
+let deepCopy (map: array<array<char>>) = map |> Array.map Array.copy
+
+// let findLoops (map: array<array<char>>) (guardRow: int, guardCol: int)=
+//     let setObstruction row col = Array.set map[row] col '#'
+//     let unsetObstruction row col = Array.set map[row] col '.'
+//     let rec loopsInRow startCol row  =
+//         if startCol >= map[0].Length
+//         then 0
+//         else if map[row][startCol] = '.'
+//         then
+//             setObstruction row startCol
+//             let hasLoop = isLoop (deepCopy map) (guardRow, guardCol) Set.empty
+//             unsetObstruction row startCol 
+//             (if hasLoop then 1 else 0) + (loopsInRow (startCol + 1) row)
+//         else
+//             loopsInRow (startCol + 1) row
+//     [0..map.Length - 1] |> List.map (loopsInRow 0) |> List.sum
+
+let countLoops (map: array<array<char>>) (guardRow: int, guardCol: int) (positions: list<int * int>) =
+    let isLoopHelper position =
+        let newMap = deepCopy map
+        Array.set newMap[fst position] (snd position) '#'
+        // printfn "Setting map at %i, %i to obstruction" (fst position) (snd position)
+        // printfn "%A" newMap
+        let res = isLoop newMap (guardRow, guardCol) Set.empty
+        if res then printfn "%A" position
+        res
+    positions |> List.filter isLoopHelper |> List.length
+
 let map = parseInput "input.dat"
-map |> findGuard |> printfn "%A"
-let newMap = match findGuard map with
-                | Some guardPos -> Some (moveGuard map guardPos)
-                | None -> None
-match newMap with
-| Some m -> printfn "%A" m
-| None -> printfn "whoops"
-match newMap with
-| Some m -> printfn "%A" (countXs m)
-| None -> printfn "whoops"
+
+match findGuard map with
+| Some guardPos -> 
+    let newMap = moveGuard (deepCopy map) guardPos
+    countXs newMap |> part1
+    // printfn "%A" map
+    // printfn "%A" newMap
+
+    let positions = [for row in [0..map.Length - 1] do for col in [0..map[0].Length - 1] -> (row, col)] 
+                    |> List.filter (fun (row, col) -> map[row][col] = '.')
+    // let positions = [(6, 3)]
+    // printfn "positions: %A" positions
+    // printfn "%A" map
+    countLoops map guardPos positions |> part2
+| None -> printfn "No guard found"
