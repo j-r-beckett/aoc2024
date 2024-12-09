41f89077f6f50d3f283018e2f928a9e9586b9299
jeckett <jeckett@cs.washington.edu>
2024-12-08 23:30:56 -0800

    Format

--- 9/Program.fs
+++ 9/Program.fs
@@ -2,9 +2,10 @@
 
 let getBlocks (diskMap: list<int>) : list<Option<int>> =
     let helper (index, value) =
-        if index % 2 <> 0
-        then [for _ in [0..value - 1] -> None]
-        else [for _ in [0..value - 1] -> Some (int ((double index) / 2.0))]
+        if index % 2 <> 0 then
+            [ for _ in [ 0 .. value - 1 ] -> None ]
+        else
+            [ for _ in [ 0 .. value - 1 ] -> Some(int ((double index) / 2.0)) ]
 
     diskMap |> List.indexed |> List.map helper |> List.concat
 
@@ -21,57 +22,91 @@ let readDiskmap filename =
     readlines filename |> List.head |> Seq.toList |> List.map (string >> int)
 
 let compressBlocks (blocks: list<option<int>>) =
-    let isSome option = match option with | None -> false | Some _ -> true
+    let isSome option =
+        match option with
+        | None -> false
+        | Some _ -> true
+
     let isNone = not << isSome
     let numEmpty = blocks |> List.filter isNone |> List.length
-    let fillers = blocks[blocks.Length - numEmpty..] |> List.filter isSome |> List.rev
+    let fillers = blocks[blocks.Length - numEmpty ..] |> List.filter isSome |> List.rev
+
     let helper (numEmptiesSoFar: int, resultSoFar: list<option<int>>) (value: option<int>) =
         match value with
-        | Some n -> numEmptiesSoFar, resultSoFar @ [Some n]
-        | None -> numEmptiesSoFar + 1, resultSoFar @ [fillers[numEmptiesSoFar]]
-        // 0, resultSoFar @ [Some 5]
-    (blocks[..blocks.Length - numEmpty - 1]
-    |> List.fold helper (0, []) |> snd) @ [for _ in [0..numEmpty - 1] -> None]
+        | Some n -> numEmptiesSoFar, resultSoFar @ [ Some n ]
+        | None -> numEmptiesSoFar + 1, resultSoFar @ [ fillers[numEmptiesSoFar] ]
+    // 0, resultSoFar @ [Some 5]
+    (blocks[.. blocks.Length - numEmpty - 1] |> List.fold helper (0, []) |> snd)
+    @ [ for _ in [ 0 .. numEmpty - 1 ] -> None ]
 
-type File = {NumBlocks: int; Id: int}
-type FreeSpace = {NumFreeBlocks: int; Files: list<File>}
+type File = { NumBlocks: int; Id: int }
 
-type DiskLocation = 
-| File of file: File
-| FreeSpace of freeSpace: FreeSpace
+type FreeSpace =
+    { NumFreeBlocks: int
+      Files: list<File> }
 
-let getDiskLocations (diskMap: list<int>) = 
+type DiskLocation =
+    | File of file: File
+    | FreeSpace of freeSpace: FreeSpace
+
+let getDiskLocations (diskMap: list<int>) =
     let helper (index, value) =
-        if index % 2 <> 0
-        then FreeSpace {NumFreeBlocks = value; Files = []}
-        else File {NumBlocks = value; Id = (int ((double index) / 2.0))}
+        if index % 2 <> 0 then
+            FreeSpace { NumFreeBlocks = value; Files = [] }
+        else
+            File
+                { NumBlocks = value
+                  Id = (int ((double index) / 2.0)) }
 
-    diskMap |> List.indexed |> List.map helper 
+    diskMap |> List.indexed |> List.map helper
 
 let compressFiles (diskLocations: list<DiskLocation>) =
     let diskLocations = diskLocations |> List.toArray
-    for i in [0..diskLocations.Length - 1] |> List.rev do
+
+    for i in [ 0 .. diskLocations.Length - 1 ] |> List.rev do
         match diskLocations[i] with
         | FreeSpace freeSpace -> ()
-        | File file -> 
-            let jOption: option<int> = Array.tryFindIndex (fun diskLoc -> match diskLoc with | FreeSpace freeSpace -> freeSpace.NumFreeBlocks >= file.NumBlocks | _ -> false) diskLocations[0..i-1]
+        | File file ->
+            let jOption: option<int> =
+                Array.tryFindIndex
+                    (fun diskLoc ->
+                        match diskLoc with
+                        | FreeSpace freeSpace -> freeSpace.NumFreeBlocks >= file.NumBlocks
+                        | _ -> false)
+                    diskLocations[0 .. i - 1]
+
             match jOption with
-            | Some j -> 
+            | Some j ->
                 match diskLocations[j] with
                 | File _ -> raise (System.InvalidOperationException "expected free space")
                 | FreeSpace freeSpace ->
-                    Array.set diskLocations j  (FreeSpace {NumFreeBlocks = freeSpace.NumFreeBlocks - file.NumBlocks; Files = freeSpace.Files @ [file]})
-                    Array.set diskLocations i (FreeSpace {NumFreeBlocks = file.NumBlocks; Files = []})
+                    Array.set
+                        diskLocations
+                        j
+                        (FreeSpace
+                            { NumFreeBlocks = freeSpace.NumFreeBlocks - file.NumBlocks
+                              Files = freeSpace.Files @ [ file ] })
+
+                    Array.set
+                        diskLocations
+                        i
+                        (FreeSpace
+                            { NumFreeBlocks = file.NumBlocks
+                              Files = [] })
             | None -> ()
 
-    let helper diskLocation = 
-        let processFile file = [for _ in [0..file.NumBlocks - 1] -> Some file.Id]
+    let helper diskLocation =
+        let processFile file =
+            [ for _ in [ 0 .. file.NumBlocks - 1 ] -> Some file.Id ]
+
         match diskLocation with
         | File file -> processFile file
-        | FreeSpace freeSpace -> (freeSpace.Files |> List.map processFile |> List.concat) @ [for _ in [0..freeSpace.NumFreeBlocks - 1] -> None]
+        | FreeSpace freeSpace ->
+            (freeSpace.Files |> List.map processFile |> List.concat)
+            @ [ for _ in [ 0 .. freeSpace.NumFreeBlocks - 1 ] -> None ]
 
     diskLocations |> Array.toList |> List.map helper |> List.concat
-    
+
 
 let computeChecksum (blocks: list<option<int>>) : int64 =
     blocks
