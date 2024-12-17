bb79fa2ebed3db88b8f706fa0751c51ce08acd7c
jeckett <jeckett@cs.washington.edu>
2024-12-16 23:06:11 -0800

    Try sped-up brute force, still too slow

--- 17/Program.fs
+++ 17/Program.fs
@@ -88,46 +88,60 @@ let cdv state operand =
         C = state.A / (pown 2 combo)
         InstructionPointer = state.InstructionPointer + 2 }
 
+let rec advanceState (state: State) (program: Program) =
+    if state.InstructionPointer >= program.Length then
+        None
+    else
+        let opcode, operand =
+            program[state.InstructionPointer], program[state.InstructionPointer + 1]
 
-let evaluate (startState: State) (program: Program) =
-    let rec execute (state: State) =
-        if state.InstructionPointer >= program.Length then
-            []
+        if opcode = 5 then
+            let nextState, outValue = out state operand
+            Some (nextState, Some outValue)
         else
-            let opcode, operand =
-                program[state.InstructionPointer], program[state.InstructionPointer + 1]
+            let nextState =
+                match opcode with
+                | 0 -> adv state operand
+                | 1 -> bxl state operand
+                | 2 -> bst state operand
+                | 3 -> jnz state operand
+                | 4 -> bxc state // bxc does not take an operand argument
+                // skip 5
+                | 6 -> bdv state operand
+                | 7 -> cdv state operand
+                | _ -> raise (ArgumentException(sprintf "Unknown opcode %i" opcode))
 
-            if opcode = 5 then
-                let nextState, outValue = out state operand
-                [ outValue ] @ execute nextState
-            else
-                let nextState =
-                    match opcode with
-                    | 0 -> adv state operand
-                    | 1 -> bxl state operand
-                    | 2 -> bst state operand
-                    | 3 -> jnz state operand
-                    | 4 -> bxc state // bxc does not take an operand argument
-                    // skip 5
-                    | 6 -> bdv state operand
-                    | 7 -> cdv state operand
-                    | _ -> raise (ArgumentException(sprintf "Unknown opcode %i" opcode))
+            advanceState nextState program
 
-                execute nextState
+let rec evaluate (startState: State) (program: Program) =
+    match advanceState startState program with
+    | None -> []
+    | Some (nextState, outputOption) ->
+        match outputOption with
+        | None -> evaluate nextState program
+        | Some output -> [output] @ evaluate nextState program
 
-    execute startState
+let rec producesTargetOutput (startState: State) (program: Program) (targetOutput: list<int>) =
+    match advanceState startState program with
+    | None -> targetOutput.IsEmpty
+    | Some (nextState, outputOption) ->
+        match outputOption with
+        | None -> targetOutput.IsEmpty
+        | Some output ->  
+            if targetOutput.Length > 0 && output = targetOutput[0]
+            then producesTargetOutput nextState program targetOutput[1..]
+            else false
 
 let findSmallestA state program =
-    let rec helper a uniqueResults =
-        if a % 100000 = 0
-        then printfn "%i, num unique results: %i" a (Set.count uniqueResults)
+    let rec helper a  =
+        if a % 1000000 = 0
+        then printfn "%i, %A" a (evaluate {state with A = a} program)
 
-        let output = evaluate {state with A = a} program
-        if output = program
+        if producesTargetOutput {state with A = a} program program
         then a
-        else helper (a + 1) (Set.add output uniqueResults)
+        else helper (a + 1)
     
-    helper 1 Set.empty
+    helper 1
 
 let startState, program = readInput "input.dat"
 // 207303137 incorrect
